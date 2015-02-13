ed7f233af5a562315bb9269f9d5d20d4ee4b85f3
Sebastian Thiel <byronimo@gmail.com>
2015-02-13 18:27:42 +0100

    added some documentation

--- string.go
+++ string.go
@@ -1,5 +1,8 @@
+// This package contains various string transformation functions
 package string
 
+// Reverse returns reverses the input string and returns it.
+// s will not be altered.
 func Reverse(s string) string {
 	c := []rune(s)
 	for i := 0; i < len(c)/2; i++ {
